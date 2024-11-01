75c0fdccc8c488b81cb3489488c8bfccd1d588af
Coded-Adil <itsadil2022@gmail.com>
2024-11-01 14:39:45 +0600

    NavBar Created and router seted up

---
+++ src/components/DashBoard/DashBoard.jsx
@@ -0,0 +1,9 @@
+const DashBoard = () => {
+    return (
+        <div>
+            <h2 className="text-5xl">Dham Dham Dhash Dashboard</h2>
+        </div>
+    );
+};
+
+export default DashBoard;
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,25 @@ import {
 } from "react-router-dom";
 import './index.css'
 import Root from './components/Root/Root';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import Home from './components/Home/Home';
+import DashBoard from './components/DashBoard/DashBoard';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: 'dashboard',
+        element: <DashBoard></DashBoard>
+      }
+    ]
   },
 ]);
 
